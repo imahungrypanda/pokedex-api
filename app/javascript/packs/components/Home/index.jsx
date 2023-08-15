0daecf64864094b580ccdf53cffa9b1e6848b404
imahungrypanda <steve@goco.io>
2023-08-15 08:18:56 -0500

    Basic react app running

---
+++ app/javascript/packs/components/Home/index.jsx
@@ -0,0 +1,18 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+
+const { Content, Footer } = Layout
+
+export default () => (
+  // <Layout className='layout'>
+  //   {/* <Header /> */}
+  //   <Content style={{ padding: '0 50px' }}>
+  //     <div className='site-layout-content' style={{ margin: '100px auto' }}>
+  //       <h1>Pokedex</h1>
+  //     </div>
+  //   </Content>
+  //   <Footer style={{ textAlign: 'center' }}>Honeybadger ©2020.</Footer>
+  // </Layout>
+  <div>Hello</div>
+)
---
+++ app/javascript/packs/components/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import Home from './Home'
+// import 'antd/dist/antd.css'
+
+export default () => <Home />
