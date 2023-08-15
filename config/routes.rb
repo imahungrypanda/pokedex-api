2ba9d7051c55fa284413344819a5c7e7066b7d33
imahungrypanda <steve@goco.io>
2023-08-15 08:19:15 -0500

    Send react files back

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :pokemons
+  namespace :api do
+    namespace :v1 do
+      get 'pokemons/index'
+      post 'pokemons/create'
+      delete 'pokemons/:id', to: 'pokemons#destroy'
+    end
+  end
+
+  root 'pokemons#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
