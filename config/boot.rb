bf286547f65e36167dc07ca9f8ec2b3465034333
imahungrypanda <steve@goco.io>
2025-02-26 08:53:12 -0700

    Bump to ruby 3.3.0

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'yaml'
+module YAML
+  class << self
+    alias_method :original_safe_load, :safe_load
+    def safe_load(source, **options)
+      options[:aliases] = true
+      original_safe_load(source, **options)
+    end
+  end
+end
+
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
